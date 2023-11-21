6f4324a6b6f6977e92d0f60e2faaf685e0b2dc0c
Jada Walters <jada.walters@mymona.uwi.edu>
2023-11-21 02:24:43 -0500

    Exercise 5 inclomplete

--- world.js
+++ world.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const lookupBtn = document.getElementById("lookup");
     var countryInput = document.getElementById("country");
     const resultDiv = document.getElementById("result");
+    const citiesBtn = document.getElementById("cities");
 
     lookupBtn.addEventListener("click", function(event) {
         event.preventDefault();
@@ -17,4 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
         xhr.open("GET", "world.php?country=" + encodeURIComponent(country), true);
         xhr.send();
     });
+
+    // citiesBtn.addEventListener("click", function(event) {
+    //     event.preventDefault();
+    //     var country = countryInput.value;
+    //     var xhr = new XMLHttpRequest();
+
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState == 4 && xhr.status == 200) {
+    //             resultDiv.innerHTML = xhr.responseText;
+    //         }
+    //     }; 
+        
+    //     xhr.open("GET", "world.php?country=" + encodeURIComponent(country), true);
+    //     xhr.send();
+    // });
 });
